a69f184747ceed1698b2d4a636a6042b3d62da08
lordfire2221 <92425429+lordfire2221@users.noreply.github.com>
2024-06-20 00:35:58 +0200

    four commit

---
+++ src/environement/environement.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase:  {
+  apiKey: "AIzaSyBOfmIUjXSCCwp8-aA1a2e8T3n1JhId0GI",
+  authDomain: "inovmind-39a70.firebaseapp.com",
+  projectId: "inovmind-39a70",
+  storageBucket: "inovmind-39a70.appspot.com",
+  messagingSenderId: "559607594775",
+  appId: "1:559607594775:web:7e4cf99fb1ff4a07c80152",
+  measurementId: "G-MXM8TDSLSH"
+  }
+};
